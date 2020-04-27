147810bd3d3fa05841f7e2fdf669f885a9c2bb69
Garrett Trott <garretttrott@gmail.com>
2020-04-26 19:29:46 -0700

    add attributes to search form

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,23 @@ const searchForm = document.createElement("form");
 const searchInput = document.createElement("input");
 const submitButton = document.createElement("input");
 
+// Add search form attributes
+searchForm.setAttribute("action", "#");
+searchForm.setAttribute("method", "get");
+
+searchInput.setAttribute("type", "search");
+searchInput.setAttribute("id", "search-input");
+searchInput.setAttribute("class", "search-input");
+searchInput.setAttribute("placeholder", "Search...");
+
+submitButton.setAttribute("type", "submit");
+submitButton.setAttribute("value", "&#x1F50D;");
+submitButton.setAttribute("id", "search-submit");
+submitButton.setAttribute("class", "search-submit");
+
 // append search elements
 searchDiv.appendChild(searchForm);
 searchForm.appendChild(searchInput);
 searchForm.appendChild(submitButton);
+
+// Hepler funtions
