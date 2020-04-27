11a4b1dfc08dc0ce30d453c1fa2823106203233d
Garrett Trott <garretttrott@gmail.com>
2020-04-27 01:05:39 -0700

    refactor search elements

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,14 @@ searchForm.setAttribute("action", "#");
 searchForm.setAttribute("method", "get");
 
 searchInput.setAttribute("type", "search");
-searchInput.setAttribute("id", "search-input");
-searchInput.setAttribute("class", "search-input");
+searchInput.id = "search-input";
+searchInput.classList.add = "search-input";
 searchInput.setAttribute("placeholder", "Search...");
 
 submitButton.setAttribute("type", "submit");
-submitButton.setAttribute("value", "&#x1F50D;");
-submitButton.setAttribute("id", "search-submit");
-submitButton.setAttribute("class", "search-submit");
+submitButton.value = "&#x1F50D;";
+submitButton.id = "search-submit";
+submitButton.classList.add("search-submit");
 
 // append search elements
 searchDiv.appendChild(searchForm);
@@ -27,10 +27,27 @@ searchForm.appendChild(submitButton);
 // FETCH Functions
 function fetchData(url) {
   return fetch(url)
+    .then(checkStatus)
     .then((res) => res.json())
     .catch((error) =>
       console.log("Looks like there was in issue with your request...", error)
     );
 }
 
-// Helper funtions
+// Helper functions
+
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function generateCard()
+
+
+// Get 12 Random Users from API
+fetchData("https://randomuser.me/api/?results=12").then((data) =>
+  console.log(data.results)
+);
