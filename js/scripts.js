960cb18013dafdd5ad85957c7173d745db99bdb7
Garrett Trott <garretttrott@gmail.com>
2020-04-30 01:33:55 -0700

    refactoring generateGallery, adds createDirecory

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+let directory = null;
 const searchDiv = document.querySelector(".search-container");
 const galleryDiv = document.querySelector("#gallery");
 
@@ -18,13 +19,35 @@ function fetchData(url) {
     );
 }
 
-// Helper functions
+//------- Helper functions ---------
+//----------------------------------
+
 function checkStatus(response) {
   if (response.ok) {
     return Promise.resolve(response);
   } else {
     return Promise.reject(new Error(response.statusText));
   }
+}
+
+function createDirectory(data) {
+  let employeeArray = [];
+
+  data.results.forEach((employee) => {
+    const person = {
+      image: employee.picture.large,
+      name: `${employee.name.first} ${employee.name.last}`,
+      email: employee.email,
+      cityState: `${employee.location.city}, ${employee.location.state}`,
+      phone: employee.phone,
+      street: `${employee.location.street.number} ${employee.location.street.name}`,
+      zip: employee.location.postcode,
+      birthday: dobToDate(employee.dob.date),
+    };
+    employeeArray.push(person);
+  });
+  directory = employeeArray;
+  return employeeArray;
 }
 
 //Turns API DOB string to date string *ex: 02/22/12
@@ -56,46 +79,56 @@ function generateCard(image, name, email, cityState) {
   return cardHTML;
 }
 
-//function that generates the
-function generateModal() {}
-
 // function that pulls 12 random US employees
-function generateGallery(numbOfEmployees) {
+function generateGallery(directory) {
   let cards = "";
-  fetchData(`https://randomuser.me/api/?nat=us&results=${numbOfEmployees}`)
-    .then((data) => {
-      console.log(data);
-      for (let i = 0; i < data.results.length; i++) {
-        const employee = {
-          image: data.results[i].picture.large,
-          name: `${data.results[i].name.first} ${data.results[i].name.last}`,
-          email: data.results[i].email,
-          cityState: `${data.results[i].location.city}, ${data.results[i].location.state}`,
-          phone: data.results[i].phone,
-          street: `${data.results[i].location.street.number} ${data.results[i].location.street.name}`,
-          zip: data.results[i].location.postcode,
-          birthday: dobToDate(data.results[i].dob.date),
-        };
-
-        cards += generateCard(
-          employee.image,
-          employee.name,
-          employee.email,
-          employee.location
-        );
-      }
-      galleryDiv.innerHTML = cards;
-    })
-    .catch((error) =>
-      console.log("There was an error loading directory...", error)
+  directory.forEach((employee) => {
+    cards += generateCard(
+      employee.image,
+      employee.name,
+      employee.email,
+      employee.cityState
     );
+  });
+  galleryDiv.innerHTML = cards;
 }
 
-generateGallery(12);
+//function that generates modal markup
+function generateModal(index) {
+  const modalHTML = `
+  <div class="modal-container">
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src="${directory.results[index].picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+        <p class="modal-text">email</p>
+        <p class="modal-text cap">city</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+      </div>
+    </div>
+    <div class="modal-btn-container">
+      <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+      <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+  </div>
+  `;
+  return modalHTML;
+}
+
+fetchData(`https://randomuser.me/api/?nat=us&results=12`)
+  .then((data) => createDirectory(data))
+  .then((directory) => generateGallery(directory))
+  .catch((error) =>
+    console.log("There was an error loading directory...", error)
+  );
 
 // Event handlers
 
-const employeeCards = document.querySelectorAll(".card");
+const employeeCards = document.querySelector("#gallery");
 const searchInput = document.querySelector("#search-input");
 const searchButton = document.querySelector("#search-submit");
 
