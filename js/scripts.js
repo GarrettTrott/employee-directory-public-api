abe005ae22ea09703d8e05cb8c7bdd9a9d665f3a
Garrett Trott <garretttrott@gmail.com>
2020-05-03 23:51:41 -0700

    adds Prev and Next modal events

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@ let directory = null;
 const directorySize = 12;
 const searchDiv = document.querySelector(".search-container");
 const galleryDiv = document.querySelector("#gallery");
+const noResults = document.createElement("H2");
+noResults.textContent = "No Search Results....";
 
 searchDiv.innerHTML = `
-  <form action="#" method="get">
-    <input type="search" id="search-input" class="search-input" placeholder="Search...">
-    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-  </form>
+<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>
 `;
 
 // FETCH Functions
@@ -31,9 +33,9 @@ function checkStatus(response) {
   }
 }
 
+//
 function createDirectory(data) {
   let employeeArray = [];
-
   data.results.forEach((employee) => {
     const person = {
       image: employee.picture.large,
@@ -51,14 +53,12 @@ function createDirectory(data) {
   return employeeArray;
 }
 
-//Turns API DOB string to date string *ex: 02/22/12
+//Turns API DOB string to date string *ex: "02/22/12"
 function dobToDate(dob) {
   const strArray = dob.split("");
-
   const year = strArray.slice(0, 4).join("");
   const month = strArray.slice(5, 7).join("");
   const day = strArray.slice(8, 10).join("");
-
   const date = [month, "/", day, "/", year].flat().join("");
   return date;
 }
@@ -68,16 +68,16 @@ function generateGallery(directory) {
   directory.forEach((employee) => {
     cards += `
     <div class="card">
-      <div class="card-img-container">
-        <img class="card-img" src="${employee.image}" alt="profile picture">
-      </div>
-      <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${employee.name}</h3>
-        <p class="card-text">${employee.email}</p>
-        <p class="card-text cap">${employee.cityState}</p>
-      </div>
+    <div class="card-img-container">
+    <img class="card-img" src="${employee.image}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+    <h3 id="name" class="card-name cap">${employee.name}</h3>
+    <p class="card-text">${employee.email}</p>
+    <p class="card-text cap">${employee.cityState}</p>
+    </div>
     </div>  
-  `;
+    `;
   });
   galleryDiv.innerHTML = cards;
 }
@@ -89,21 +89,21 @@ function generateModal(index) {
 
   const modalHTML = `
   <div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-      <img class="modal-img" src="${directory[index].image}" alt="profile picture">
-      <h3 id="name" class="modal-name cap">${directory[index].name}</h3>
-      <p class="modal-text">${directory[index].email}</p>
-      <p class="modal-text cap">${directory[index].cityState}</p>
-      <hr>
-      <p class="modal-text">${directory[index].phone}</p>
-      <p class="modal-text">${directory[index].street}</p>
-      <p class="modal-text">${directory[index].birthday}</p>
-    </div>
+  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+  <div class="modal-info-container">
+  <img class="modal-img" src="${directory[index].image}" alt="profile picture">
+  <h3 id="name" class="modal-name cap">${directory[index].name}</h3>
+  <p class="modal-text">${directory[index].email}</p>
+  <p class="modal-text cap">${directory[index].cityState}</p>
+  <hr>
+  <p class="modal-text">${directory[index].phone}</p>
+  <p class="modal-text">${directory[index].street}</p>
+  <p class="modal-text">${directory[index].birthday}</p>
+  </div>
   </div>
   <div class="modal-btn-container">
-    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+  <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+  <button type="button" id="modal-next" class="modal-next btn">Next</button>
   </div>
   `;
 
@@ -111,9 +111,29 @@ function generateModal(index) {
   galleryDiv.insertAdjacentElement("afterend", modalContainer);
 
   const modalClose = document.querySelector("#modal-close-btn");
+  const modalButtons = document.querySelector(".modal-btn-container");
 
   modalClose.addEventListener("click", () => {
     modalContainer.remove();
+  });
+
+  modalButtons.addEventListener("click", (e) => {
+    let currentIndex = index;
+    if (e.target.textContent === "Prev") {
+      modalContainer.remove();
+      if (currentIndex === 0) {
+        generateModal(directorySize - 1);
+      } else if (currentIndex <= directorySize - 1) {
+        generateModal(currentIndex - 1);
+      }
+    } else if (e.target.textContent === "Next") {
+      modalContainer.remove();
+      if (currentIndex === directorySize - 1) {
+        generateModal(0);
+      } else if (currentIndex <= directorySize - 1) {
+        generateModal(currentIndex + 1);
+      }
+    }
   });
 }
 
@@ -124,17 +144,16 @@ fetchData(`https://randomuser.me/api/?nat=us&results=${directorySize}`)
     console.log("There was an error loading directory...", error)
   );
 
-// Event handlers
-
+// Event handlers //
 const searchInput = document.querySelector("#search-input");
 const searchButton = document.querySelector("#search-submit");
-const employeeCards = document.querySelector("#gallery");
 
 searchButton.addEventListener("click", (e) => {});
 
 searchInput.addEventListener("keyup", () => {});
 
-employeeCards.addEventListener("click", (e) => {
+// When
+galleryDiv.addEventListener("click", (e) => {
   const cards = document.querySelectorAll(".card");
   for (let i = 0; i < directory.length; i++) {
     if (
