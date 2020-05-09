102c1327a631460b4ac03c60b4a0cdd20b925ee4
Garrett Trott <garretttrott@gmail.com>
2020-05-08 23:30:35 -0700

    refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 let directory = null;
 const directorySize = 12;
+
 const searchDiv = document.querySelector(".search-container");
-const galleryDiv = document.querySelector("#gallery");
-const noResults = document.createElement("h3");
-
-noResults.textContent = "No Search Results....";
-noResults.style.display = "none";
-
 searchDiv.innerHTML = `
   <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
   </form>
-`;
+  `;
+const searchButton = document.querySelector("#search-submit");
+const searchInput = document.querySelector("#search-input");
+
+const galleryDiv = document.querySelector("#gallery");
+const noResults = document.createElement("h3");
+noResults.textContent = "No Search Results....";
+noResults.style.display = "none";
+
+const modalContainer = document.createElement("div");
+modalContainer.classList.add("modal-container");
+modalContainer.style.display = "none";
+galleryDiv.insertAdjacentElement("afterend", modalContainer);
 
 function searchNames(input) {
   const galleryNames = document.querySelectorAll(".card-name");
@@ -89,87 +96,60 @@ function generateGallery(directory) {
   let cards = "";
   directory.forEach((employee) => {
     cards += `
-    <div class="card">
-      <div class="card-img-container">
-        <img class="card-img" src="${employee.image}" alt="profile picture">
-      </div>
-      <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${employee.name}</h3>
-        <p class="card-text">${employee.email}</p>
-      <p class="card-text cap">${employee.city}</p>
-      </div>
-    </div>  
-    `;
+      <div class="card">
+        <div class="card-img-container">
+          <img class="card-img" src="${employee.image}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${employee.name}</h3>
+          <p class="card-text">${employee.email}</p>
+          <p class="card-text cap">${employee.city}</p>
+        </div>
+      </div>  
+      `;
   });
   galleryDiv.innerHTML = cards;
   galleryDiv.appendChild(noResults);
 }
 
 //function that generates modal markup
-function generateModal(index) {
-  const modalContainer = document.createElement("div");
-  modalContainer.classList.add("modal-container");
-
-  const modalHTML = `
-  <div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-      <img class="modal-img" src="${directory[index].image}" alt="profile picture">
-      <h3 id="name" class="modal-name cap">${directory[index].name}</h3>
-      <p class="modal-text">${directory[index].email}</p>
-      <p class="modal-text cap">${directory[index].city}</p>
-    <hr>
-      <p class="modal-text">${directory[index].phone}</p>
-      <p class="modal-text">${directory[index].street}, ${directory[index].state} ${directory[index].zip}</p>
-      <p class="modal-text">Birthday: ${directory[index].birthday}</p>
-    </div>
-  </div>
-  <div class="modal-btn-container">
-    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-  </div>
+function generateModals(directory) {
+  let modals = "";
+  directory.forEach((employee) => {
+    modals += `
+      <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+          <img class="modal-img" src="${employee.image}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${employee.name}</h3>
+          <p class="modal-text">${employee.email}</p>
+          <p class="modal-text cap">${employee.city}</p>
+          <hr>
+          <p class="modal-text">${employee.phone}</p>
+          <p class="modal-text">${employee.street}, ${employee.state} ${employee.zip}</p>
+          <p class="modal-text">Birthday: ${employee.birthday}</p>
+        </div>
+      </div>
+      `;
+  });
+  modals += `
+      <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+      </div>
   `;
-
-  modalContainer.innerHTML = modalHTML;
-  galleryDiv.insertAdjacentElement("afterend", modalContainer);
-
-  const modalClose = document.querySelector("#modal-close-btn");
-  const modalButtons = document.querySelector(".modal-btn-container");
-
-  modalClose.addEventListener("click", () => {
-    modalContainer.remove();
-  });
-
-  modalButtons.addEventListener("click", (e) => {
-    let currentIndex = index;
-    if (e.target.textContent === "Prev") {
-      modalContainer.remove();
-      if (currentIndex === 0) {
-        generateModal(directorySize - 1);
-      } else if (currentIndex <= directorySize - 1) {
-        generateModal(currentIndex - 1);
-      }
-    } else if (e.target.textContent === "Next") {
-      modalContainer.remove();
-      if (currentIndex === directorySize - 1) {
-        generateModal(0);
-      } else if (currentIndex <= directorySize - 1) {
-        generateModal(currentIndex + 1);
-      }
-    }
-  });
+  modalContainer.innerHTML = modals;
 }
 
 fetchData(`https://randomuser.me/api/?nat=us&results=${directorySize}`)
   .then((data) => (directory = createDirectory(data)))
   .then((directory) => generateGallery(directory))
+  .then(() => generateModals(directory))
   .catch((error) =>
     console.log("There was an error loading directory...", error)
   );
 
 // Event handlers //
-const searchInput = document.querySelector("#search-input");
-const searchButton = document.querySelector("#search-submit");
 
 searchButton.addEventListener("click", (e) => {
   searchNames(searchInput);
@@ -179,16 +159,54 @@ searchInput.addEventListener("keyup", (e) => {
   searchNames(searchInput);
 });
 
-// When employee card is clicked generate modal
+//When employee card is clicked generate modal
 galleryDiv.addEventListener("click", (e) => {
+  let selectedName = "";
+  const modals = document.querySelectorAll(".modal");
   const cards = document.querySelectorAll(".card");
-  for (let i = 0; i < directory.length; i++) {
-    if (
-      e.target === cards[i] ||
-      e.target.parentNode === cards[i] ||
-      e.target.parentNode.parentNode === cards[i]
-    ) {
-      generateModal(i);
+
+  console.log(e.target);
+  cards.forEach((employee) => {
+    if (e.target.className === "card") {
+      selectedName = e.target.lastChild.firstChild.textContent;
+      console.log(selectedName);
+    } else if (e.target.className === "card-img-container") {
+      selectedName = e.target.nextChild.firstChild.textContent;
     }
+  });
+
+  modals.forEach((employee) => {});
+});
+
+modalContainer.addEventListener("click", (e) => {
+  const modals = document.querySelectorAll(".modal");
+  if (e.target.textContent === "X") {
+    modals.forEach((modal) => {
+      modal.style.display = "none";
+    });
+    modalContainer.style.display = "none";
+  } else if (e.target.textContent === "Prev") {
+    console.log("prev");
+  } else if (e.target.textContent === "Next") {
+    console.log("next");
   }
 });
+
+// modalButtons.addEventListener("click", (e) => {
+//   let currentIndex = index;
+//     if (e.target.textContent === "Prev") {
+//       modalContainer.remove();
+//       if (currentIndex === 0) {
+//         generateModal(directorySize - 1);
+//       } else if (currentIndex <= directorySize - 1) {
+//         generateModal(currentIndex - 1);
+//       }
+//     } else if (e.target.textContent === "Next") {
+//       modalContainer.remove();
+//       if (currentIndex === directorySize - 1) {
+//         generateModal(0);
+//       } else if (currentIndex <= directorySize - 1) {
+//         generateModal(currentIndex + 1);
+//       }
+//   }
+// });
